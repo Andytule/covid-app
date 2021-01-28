7ccfda050fcf1eb2fc8f152d6afc0211d4c22a24
AndyTuLe <Andytule321@gmail.com>
2021-01-28 17:50:34 -0500

    Updates

---
+++ precache-manifest.1b11514c23fbbbd5da60fa088cefd979.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3c230470f1b69da94bc5285c49d9264",
+    "url": "/covid-app/index.html"
+  },
+  {
+    "revision": "c3ce463be41508e75af5",
+    "url": "/covid-app/static/css/main.699560f4.chunk.css"
+  },
+  {
+    "revision": "83935a3ce82fdeb15e21",
+    "url": "/covid-app/static/js/2.44873e90.chunk.js"
+  },
+  {
+    "revision": "1ef8d540b3072f991ff256dfc61c7bcd",
+    "url": "/covid-app/static/js/2.44873e90.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c3ce463be41508e75af5",
+    "url": "/covid-app/static/js/main.e9eaed8d.chunk.js"
+  },
+  {
+    "revision": "29ca50a3aed0d3c8c9ed",
+    "url": "/covid-app/static/js/runtime-main.f555aef5.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid-app/static/media/image.d7265326.png"
+  }
+]);
